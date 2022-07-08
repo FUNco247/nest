7ddd0483b94d922341a5c0635d59d16cad5c5866
FUNco <crispy.alc@gmail.com>
2022-07-08 22:11:58 +0900

    seperate service code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,43 +10,34 @@ import {
   Query,
 } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from './entities/app.user';
 
 @Controller('user')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get() // router.get in expressJS and @post also availabe & decorator shoild stacked with function, never enter space!
-  getAllUser(): string {
-    return 'all user';
+  getAllUser(): User[] {
+    return this.appService.getAll();
   }
 
-  @Get('/find')
-  findUserId(@Query('name') queryKey): string {
-    return `finding user using ${queryKey} `;
+  @Post('/join')
+  createUser(@Body() newUser): void {
+    return this, this.appService.create(newUser);
   }
 
   @Get('/:id')
-  getOneUser(@Param('id') userId: string): string {
-    return `find user ${userId}`;
-  }
-
-  @Post('/register')
-  createUser(@Body() newUser): string {
-    return `user ${newUser.name} created`;
+  getOneUser(@Param('id') userId: string): User {
+    return this.appService.getOne(+userId);
   }
 
   @Patch('/patch/:id')
-  patchUser(@Param('id') userId: string, @Body() updatedUser): string {
-    return `user ${userId}'s name is changed to ${updatedUser.name}`;
-  }
-
-  @Put('/put/:id')
-  putUser(@Param('id') userId: string): string {
-    return `user ${userId} is totally updated`;
+  patchUser(@Param('id') userId: string, @Body() updatedUser): void {
+    return this.appService.update(+userId, updatedUser);
   }
 
   @Delete('/delete/:id')
-  deleteUser(@Param('id') userId: string): string {
-    return `user ${userId} is removed`;
+  deleteUser(@Param('id') userId: string): boolean {
+    return this.appService.deleteOne(+userId);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { NotFoundError } from 'rxjs';
+import { User } from './entities/app.user';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private users: User[] = [];
+
+  getAll(): User[] {
+    return this.users;
+  }
+
+  getOne(id: number): User {
+    const targetUser = this.users.find((user) => user.id === id);
+    if (!targetUser) {
+      throw new NotFoundException(`User number ${id} not found`);
+    }
+    return targetUser;
+  }
+
+  deleteOne(id: number): boolean {
+    this.users = this.users.filter((user) => user.id !== id);
+    return true;
+  }
+
+  create(newUserData: User): void {
+    this.users.push({
+      id: this.users.length + 1,
+      ...newUserData,
+    });
+  }
+
+  update(id: number, newData): void {
+    const targetUser = this.getOne(id);
+    this.deleteOne(id);
+    this.users.push({
+      ...targetUser,
+      ...newData,
+    });
   }
 }
